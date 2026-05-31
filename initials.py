fc74d39f1e03d0b38c4638fbf288d7a6f0ed59b6
Mr Gavor, Bryan Kwaku Yayra <2425402311@live.gctu.edu.gh>
2026-05-31 16:17:48 +0000

    Implement initials printing in block letters
    This program prints the initials 'B', 'Y', and 'G' in large stylized block letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,72 @@
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Bryan Kwaku Yayra Gavor
+# STUDENT ID: 2425402311
+# Email: 2425402311@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Bryan Kwaku Yayra Gavor, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials in their letters in block letters.
+
+    print()
+    print("...BYG")
+    print()
+    # B in block letters
+    b1 = "BBBBBBBBBBB."
+    b2 = "BB........BB"
+    b3 = "BB........BB"
+    b4 = "BB........BB"
+    b5 = "BBBBBBBBBBB."
+    b6 = "BB........BB"
+    b7 = "BB........BB"
+    b8 = "BB........BB"
+    b9 = "BB........BB"
+    b10 = "BBBBBBBBBBB."
+
+    # Y iblock in letters
+    y1 = "YY........YY"
+    y2 = ".YY......YY."
+    y3 = "..YY....YY.."
+    y4 = "...YY..YY..."
+    y5 = "....YYYY...."
+    y6 = ".....YY....."
+    y7 = ".....YY....."
+    y8 = ".....YY....."
+    y9 = ".....YY....."
+    y10 = ".....YY....."
+
+    # G in block letters
+    g1 = ".GGGGGGGGGG."
+    g2 = "GG........GG"
+    g3 = "GG.........."
+    g4 = "GG.........."
+    g5 = "GG.........."
+    g6 = "GG......GGGG"
+    g7 = "GG........GG"
+    g8 = "GG........GG"
+    g9 = "GG........GG"
+    g10 = ".GGGGGGGGGG."
+    
+    # Compiling and printing the letters B,Y and G with the required procedure
+    print("..." + b1 + "......." + y1 + "........" + g1 + ".....")
+    print("..." + b2 + "......." + y2 + "........" + g2 + ".....")
+    print("..." + b3 + "......." + y3 + "........" + g3 + ".....")
+    print("..." + b4 + "......." + y4 + "........" + g4 + ".....")
+    print("..." + b5 + "......." + y5 + "........" + g5 + ".....")
+    print("..." + b6 + "......." + y6 + "........" + g6 + ".....")
+    print("..." + b7 + "......." + y7 + "........" + g7 + ".....")
+    print("..." + b8 + "......." + y8 + "........" + g8 + ".....")
+    print("..." + b9 + ".....**" + y9 + "...**..." + g9 + "...**")
+    print("..." + b10 + ".....**" + y10 + "...**..." + g10 + "...**")
+
+    print()
 
 
 
